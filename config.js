13b598b23a01ea18fbf1519ae0ee331b6c277aeb
Anna Szmyga <anna.szmyga@o2.pl>
2019-05-15 11:08:32 +0200

    Write app

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '34982051221-a2nud0p3pgkifsv2ab7fte7n6ra7afi8.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '43O-L0zm9CWdWTwVRD6cX299',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
